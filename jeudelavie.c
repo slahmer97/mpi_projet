ebcb5362ecea48c0c33602d90e3666ef4a20a018
sidahmedlahmer <seyyidahmed.lahmer@etu.unistra.fr>
2019-03-22 02:10:10 +0100

    Final

--- jeudelavie.c
+++ jeudelavie.c
@@ -8,13 +8,15 @@
 
 #ifdef __linux__
 #include <openmpi-x86_64/mpi.h>
+#include <memory.h>
+
 #elif  __APPLE__
 #include <mpi.h>
 #endif
 
 // hauteur et largeur de la matrice
-#define HM 12
-#define LM 8
+#define HM 1200
+#define LM 800
 
 // nombre total d'itérations
 #define ITER 10001
@@ -33,13 +35,13 @@ void init(Tab);
 // calcule une nouveau tableau de cellules à partir de l'ancien
 // - paramètres : ancien, nouveau
 void calcnouv(Tab, Tab);
+void calcnouv_l_i_f(Tab,Tab);
 
 // variables globales : pas de débordement de pile
 Tab t1, t2;
 Tab tsauvegarde[1+ITER/SAUV];
-
-int next = 0;
-int size,rank,local_rows_num;
+void exchange_and_calculate(int i);
+int size,rank,local_rows_num,next_n,previous_n;
 int main(int argc,char**argv)
 {
  // struct timeval tv_init, tv_beg, tv_end, tv_save;
@@ -47,10 +49,8 @@ int main(int argc,char**argv)
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Status mpi_status;
-  MPI_Request mpi_send_req1,mpi_send_req2,mpi_recv_req1,mpi_recv_req2;
-  int next_n = (rank == size-1)?0:rank+1;
-  int previous_n = (rank == 0)?size-1:rank-1;
+  next_n = (rank == size-1)?0:rank+1;
+  previous_n = (rank == 0)?size-1:rank-1;
   int* send_count = NULL;
   int *displacement = NULL;
   int reminder = HM%size;
@@ -63,11 +63,7 @@ int main(int argc,char**argv)
   //gettimeofday( &tv_init, NULL);
 
   if(rank == 0){
-
-    printf("size  : %d\n",size);
     init(t2);
-      printf("Next : %d\n",next_n);
-      printf("Previous : %d\n",previous_n);
     /*
     send_count = malloc(sizeof(int)*size);
     displacement = malloc(sizeof(int)*size);
@@ -85,57 +81,45 @@ int main(int argc,char**argv)
     }
 */
   }
+//  if(rank == size-1){
+//    memset(&t2[local_rows_num+1],0,LM);
+//  }
 
 
  // MPI_Scatterv(t1,send_count,displacement,MPI_CHAR,&t1,local_rows_num,MPI_CHAR,0,MPI_COMM_WORLD);
   MPI_Scatter(t2,local_rows_num*LM,MPI_CHAR,&t1[1],local_rows_num*LM,MPI_CHAR,0,MPI_COMM_WORLD);
 
-  if(rank != 0)
-    MPI_Isend(&t1[1],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_send_req1);
-  if(rank != size-1)
-    MPI_Isend(&t1[local_rows_num],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_send_req2);
-  if(rank != 0)
-    MPI_Irecv(&t1[0],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_recv_req1);
-  if(rank != size-1)
-    MPI_Irecv(&t1[local_rows_num+1],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_recv_req2);
+for(int i=0;i<ITER;i++){
 
+  exchange_and_calculate(i);
+  if(i % SAUV == 0){
+      if(rank == 0){
+          printf("--------save(%d)-------\n",i);
+         MPI_Gather(&t1[1],local_rows_num*LM,MPI_CHAR,&tsauvegarde[i/SAUV],local_rows_num*LM,MPI_CHAR,0,MPI_COMM_WORLD);
 
-  if(rank != 0)
-     MPI_Wait(&mpi_send_req1,MPI_STATUS_IGNORE);
-  if(rank != size-1)
-    MPI_Wait(&mpi_send_req2,MPI_STATUS_IGNORE);
-  if(rank != 0)
-    MPI_Wait(&mpi_recv_req1,MPI_STATUS_IGNORE);
-  if(rank != size-1)
-     MPI_Wait(&mpi_recv_req2,MPI_STATUS_IGNORE);
+      }
+      else
+          MPI_Gather(&t1[1],local_rows_num*LM,MPI_CHAR,NULL,local_rows_num*LM,MPI_CHAR,0,MPI_COMM_WORLD);
 
-
-
-
-
-
-  if(rank == 0)
-     MPI_Gather(&t1[1],local_rows_num*LM,MPI_CHAR,&tsauvegarde[next++],local_rows_num*LM,MPI_CHAR,0,MPI_COMM_WORLD);
-  else
-      MPI_Gather(&t1[1],local_rows_num*LM,MPI_CHAR,NULL,local_rows_num*LM,MPI_CHAR,0,MPI_COMM_WORLD);
-
-  if(rank == 0){
-    FILE *f = fopen("jdlv.out", "w");
-
-        fprintf(f, "------------------ sauvegarde %d ------------------\n", 0);
-        for(int x=0 ; x<HM ; x++)
-        {
-            for(int y=0 ; y<LM ; y++)
-                fprintf(f, tsauvegarde[0][x][y]?"*":"-");
-            fprintf(f, "\n");
-        }
-
-    fclose(f);
 
   }
+}
 
-
-  /*
+if(rank == 0){
+  FILE *f = fopen("test/para.out", "w");
+  for(int i=0 ; i<ITER ; i+=SAUV)
+  {
+    fprintf(f, "------------------ sauvegarde %d ------------------\n", i);
+    for(int x=0 ; x<HM ; x++)
+    {
+      for(int y=0 ; y<LM ; y++)
+        fprintf(f, tsauvegarde[i/SAUV][x][y]?"*":" ");
+      fprintf(f, "\n");
+    }
+  }
+  fclose(f);
+}
+/*
     char file[20];
   sprintf(file,"file.%d",rank);
   FILE *f = fopen(file, "w");
@@ -158,8 +142,6 @@ int main(int argc,char**argv)
 
   */
 
-
-
 /*
   //gettimeofday( &tv_beg, NULL);
   for(int i=0 ; i<ITER ; i++)
@@ -180,18 +162,7 @@ int main(int argc,char**argv)
   }
   //gettimeofday( &tv_end, NULL);
 
-  FILE *f = fopen("jdlv.out", "w");
-  for(int i=0 ; i<ITER ; i+=SAUV)
-  {
-    fprintf(f, "------------------ sauvegarde %d ------------------\n", i);
-    for(int x=0 ; x<HM ; x++)
-    {
-      for(int y=0 ; y<LM ; y++)
-        fprintf(f, tsauvegarde[i/SAUV][x][y]?"*":" ");
-      fprintf(f, "\n");
-    }
-  }
-  fclose(f);
+
   //gettimeofday( &tv_save, NULL);
 
   //printf("init : %lf s,", DIFFTEMPS(tv_init, tv_beg));
@@ -212,6 +183,7 @@ void init(Tab t)
 {
 
  // srand(time(0));
+#pragma omp prallel for
   for(int i=0 ; i<HM ; i++)
     for(int j=0 ; j<LM ; j++ )
     {
@@ -220,7 +192,7 @@ void init(Tab t)
       // t[i][j] = (i==0||j==0||i==h-1||j==l-1)?0:1;
       t[i][j] = 0;
     }
-    /*
+
   t[10][10] = 1;
   t[10][11] = 1;
   t[10][12] = 1;
@@ -234,7 +206,7 @@ void init(Tab t)
   t[56][50] = 1;
   t[56][51] = 1;
   t[56][52] = 1;
-   */
+   /*
   t[0][0] = 1;
   t[1][1] = 1;
   t[2][2] = 1;
@@ -247,7 +219,7 @@ void init(Tab t)
   t[9][2] = 1;
   t[10][3] = 1;
   t[11][7] = 1;
-
+*/
 
 }
 
@@ -288,7 +260,7 @@ int nbvois(Tab t, int i, int j)
 void calcnouv(Tab t, Tab n)
 {
 #pragma omp parallel for
-  for(int i=0 ; i<HM ; i++)
+  for(int i=2 ; i < local_rows_num ; i++)
     for(int j=0 ; j<LM ; j++)
     {
       int v = nbvois(t, i, j);
@@ -299,5 +271,76 @@ void calcnouv(Tab t, Tab n)
       else
         n[i][j] = 0;
     }
+
+}
+
+void calcnouv_l_i_f(Tab t,Tab n){
+#pragma omp prallel for
+  for(int j=0 ; j<LM ; j++)
+  {
+    int v = nbvois(t,1, j);
+    if(v==3)
+      n[1][j] = 1;
+    else if(v==2)
+      n[1][j] = t[1][j];
+    else
+      n[1][j] = 0;
+  }
+#pragma omp parallel for
+  for(int j=0 ; j<LM ; j++)
+  {
+    int v = nbvois(t,local_rows_num, j);
+    if(v==3)
+      n[local_rows_num][j] = 1;
+    else if(v==2)
+      n[local_rows_num][j] = t[local_rows_num][j];
+    else
+      n[local_rows_num][j] = 0;
+  }
+}
+
+void exchange_and_calculate(int i){
+    MPI_Request mpi_send_req1,mpi_send_req2,mpi_recv_req1,mpi_recv_req2;
+    if(i%2 == 0){
+        if(rank != 0)
+            MPI_Isend(&t1[1],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_send_req1);
+        if(rank != size-1)
+            MPI_Isend(&t1[local_rows_num],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_send_req2);
+        if(rank != 0)
+            MPI_Irecv(&t1[0],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_recv_req1);
+        if(rank != size-1)
+            MPI_Irecv(&t1[local_rows_num+1],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_recv_req2);
+        calcnouv(t1,t2);
+
+    }else{
+        if(rank != 0)
+            MPI_Isend(&t2[1],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_send_req1);
+        if(rank != size-1)
+            MPI_Isend(&t2[local_rows_num],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_send_req2);
+        if(rank != 0)
+            MPI_Irecv(&t2[0],LM,MPI_CHAR,previous_n,0,MPI_COMM_WORLD,&mpi_recv_req1);
+        if(rank != size-1)
+            MPI_Irecv(&t2[local_rows_num+1],LM,MPI_CHAR,next_n,0,MPI_COMM_WORLD,&mpi_recv_req2);
+        calcnouv(t2,t1);
+    }
+
+
+    if(rank != 0)
+        MPI_Wait(&mpi_send_req1,MPI_STATUS_IGNORE);
+    if(rank != size-1)
+        MPI_Wait(&mpi_send_req2,MPI_STATUS_IGNORE);
+    if(rank != 0)
+        MPI_Wait(&mpi_recv_req1,MPI_STATUS_IGNORE);
+    if(rank != size-1)
+        MPI_Wait(&mpi_recv_req2,MPI_STATUS_IGNORE);
+
+    //have previous next...
+    //calculate first and last line
+    if(i%2 == 0)
+    calcnouv_l_i_f(t1,t2);
+    else
+    calcnouv_l_i_f(t2,t1);
+
+
 
 }
